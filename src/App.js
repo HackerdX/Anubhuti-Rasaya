22fc019d1384cbd35d95b6b77a5f4685742e5f37
HackerdX <cooleasycodebyanmol@gmail.com>
2023-06-16 01:26:55 +0530

    Navbar Enhancement Completed, just need a touch of issues to be resolved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <h1>Anubhuti Rasaya</h1>
-      {/* Add additional components or content here */}
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" exact />
+          {/* <h1>Anubhuti Rasaya</h1> */}
+          {/* Add additional components or content here */}
+        </Routes>
+
+      </Router>
+    </>
   );
 }
+
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,53 @@
 import React, { useState } from 'react';
 import '../styles/Navbar.css';
 import logo from '../images/logo.jpg';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
 
 const Navbar = () => {
-    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [click, setClick] = useState(false);
+    let [button, setButton] = useState(true);
 
-    const toggleDrawer = () => {
-        setDrawerOpen(!drawerOpen);
+    const menuClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
     };
-
+    window.addEventListener('resize', showButton);
     return (
-        <nav>
-            <div className="navbar-container">
-                <div className="logo">
-                    <img src={logo} alt="Logo" />
-                </div>
-                <div className={`navigation ${drawerOpen ? 'open' : ''}`}>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Contact Us</a></li>
+        <>
+            <nav className='navbar'>
+                <div className="navbar-container">
+                    <Link to="/" className="logo">
+                        {/* <img src={logo} alt="Logo" /> */}
+                        TRVL <i className='fab fa-typo3'></i>
+                    </Link>
+                    <div className='menu-icon' onClick={menuClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to="/" className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/about" className='nav-links' onClick={closeMobileMenu}>About</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/products" className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/contact-us" className='nav-links' onClick={closeMobileMenu}>ContactUs</Link>
+                        </li>
                     </ul>
+                    {button && <Button buttonStyle="btn-outline">Contact Us</Button>}
                 </div>
-                <div className="drawer-icon" onClick={toggleDrawer}>
-                    <div className={`bar ${drawerOpen ? 'open' : ''}`} />
-                    <div className={`bar ${drawerOpen ? 'open' : ''}`} />
-                    <div className={`bar ${drawerOpen ? 'open' : ''}`} />
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     );
 };
-
 export default Navbar;
